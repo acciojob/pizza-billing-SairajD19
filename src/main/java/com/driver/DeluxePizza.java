c071f4d03ab5528a01d5b3673bc1fc4f4c11c6f3
SairajD19 <sairajdawate25@gmail.com>
2022-12-11 21:23:50 +0530

    Assignment 4 Changes 4

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,13 +4,15 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        addExtraCheese();
+        addExtraToppings();
         // your code goes here
-        if(isVeg){
+        /*if(isVeg){
             setPrice(getPrice()+150);
         }
         else{
             setPrice(getPrice()+200);
-        }
+        }*/
 
     }
 
