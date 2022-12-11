bbac697d6c3bc0a7f52f8780408922b4bd87de9a
SairajD19 <sairajdawate25@gmail.com>
2022-12-11 15:48:45 +0530

    Assignment 4

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,16 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+
+        if(isVeg){
+            setPrice(getPrice()+70+80);
+            //setPrice(450);
+        }
+        else{
+            setPrice(getPrice()+120+80);
+            //setPrice(600);
+        }
+
     }
+
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,27 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private String  totalBill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            setPrice(300);
+            setBill("Base Price Of The Pizza = 300\n");
+        }
+        else {
+            setPrice(400);
+            setBill("Base Price Of The Pizza = 400\n");
+        }
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     public int getPrice(){
@@ -17,18 +34,37 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        setPrice(getPrice()+80);
+        setBill(getBill()+"Extra Cheese Added = 80\n");
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            setPrice(getPrice()+70);
+            setBill(getBill()+"Extra Toppings Added = 70\n");
+        }
+        else{
+            setPrice(getPrice()+120);
+            setBill(getBill()+"Extra Toppings Added = 120\n");
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        setPrice(getPrice()+20);
+        setBill(getBill()+"Paperbag Added = 20\n");
+
     }
 
     public String getBill(){
         // your code goes here
+
         return this.bill;
+    }
+
+    public String getTotalBill() {
+        return getBill()+"Total Price = "+getPrice()+"\n";
     }
 }
